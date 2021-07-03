f6f801a71e01766ec78433552f0a2c2325fcf36d
tomascoelhocruz <tomascoelhocruz@gmail.com>
2021-07-03 23:06:20 +0100

    2

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-      "default": env.dj_db_url("DATABASE_URL")
+      "default": Env.dj_db_url("DATABASE_URL")
  }
 
 # Password validation
